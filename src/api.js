652bfcf5c941cbf94c8b77c218f43004a9851574
anik87 <anikbanerjee.87@gmail.com>
2025-04-12 21:08:16 -0400

    NOTE-022: jwt react fixf

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const API_BASE_URL = window.location.hostname === 'localhost'
   : 'https://nevernote-express.onrender.com';
 
 export async function loginUser(username, password) {
-  const response = await fetch(`${API_BASE_URL}/login`, {
+  const response = await fetch(`${API_BASE_URL}/auth/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password }),
